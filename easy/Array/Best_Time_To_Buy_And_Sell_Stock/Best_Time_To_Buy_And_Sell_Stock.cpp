4610c6dca2bc4a9a33bffc1e74f87cf4937960af
Omar Ghazi <omghazi@e3r2p1.1337.ma>
2025-02-08 23:11:36 +0100

    ADD Best Time blabla...

---
+++ easy/Array/Best_Time_To_Buy_And_Sell_Stock/Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -0,0 +1,21 @@
+c#include <vector>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        if (prices.empty()) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0; 
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+            else if (prices[i] - minPrice > maxProfit)
+                maxProfit = prices[i] - minPrice;
+        }
+        return maxProfit;
+    }
+};l
